1235aef10c78e47369eadc9d0b59409b4c243200
Andreas Mueller <andreas.mueller2207@web.de>
2016-07-10 22:01:49 +0200

    Add wakeonlan support to wake_omv.sh

--- wake_omv.sh
+++ wake_omv.sh
@@ -1,9 +1,12 @@
 #!/bin/bash
 # Author: Andreas
 # Date: 2016-03-10
-# Description: Wake up my local openmediavault NAS by Wake-On-Lan protocol (RFC
+# Description: Wake up my local openmediavault NAS by Wake-On-Lan protocol .
 #              Send two magic packets with 1 s pause in between just to make sure.
+
+# Try both wol and wakeonlan, one of them should work :-p
+wakeonlan bc:5f:f4:dd:0d:3c
 wol bc:5f:f4:dd:0d:3c
 sleep 1s
+wakeonlan bc:5f:f4:dd:0d:3c
 wol bc:5f:f4:dd:0d:3c
-
