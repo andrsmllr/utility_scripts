6db72b81ecd9a5ef0596ded6cdb365ff4021cd03
Andreas Mueller <andym2207@gmx.de>
2016-07-29 23:07:14 +0200

    Call git status only if folder contains a git repo

--- git_status.sh
+++ git_status.sh
@@ -4,11 +4,17 @@
 # Author: Andreas Mueller
 # Date:   2016-03-17
 #
-# Description: Check each subfolder if it is a git repo and print its status it.
+# Description: Check each subfolder if it is a git repo and print its status.
 #
+
 
 find . -mindepth 1 -maxdepth 1 -type d -exec sh -c '
   cd "$1";
-  echo "### Status of $1 ###";
-  git status;
+  if [ -e ./.git ]; then
+    echo "### Status of $1 ###";
+    git status;
+  else
+    echo "### Skipping $1, not a git repo ###";
+  fi;
 ' 0 {} \;
+
