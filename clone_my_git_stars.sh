6de3b243278a9a83f0e60899b0659de78bde5bcc
Andreas Mueller <andreas.mueller2207@web.de>
2017-11-08 23:31:14 +0100

    New script to checkout favorite git repos

---
+++ clone_my_git_stars.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+# Clone git repositories of interest, from github.com and other sources.
+# Comment out a line to skip a git repo.
+git clone https://github.com/ucb-bar/chisel3
+git clone https://github.com/ucb-bar/firrtl
+git clone https://github.com/tgingold/ghdl
+git clone https://github.com/rubund/graywolf
+git clone https://github.com/steveicarus/iverilog
+git clone git://opencircuitdesign.com/magic-8.1
+git clone git://opencircuitdesign.com/magic-8.2
+git clone https://github.com/rubund/debian-magic
+git clone https://github.com/Motion-Project/motion
+git clone https://github.com/ccrisan/motioneye
+git clone https://github.com/ccrisan/motioneyeos
+git clone https://github.com/jandecaluwe/myhdl
+git clone https://github.com/kevinpt/opbasm
+git clone https://github.com/openrisc/or1200
+git clone https://github.com/jimlewis/osvvm
+git clone https://github.com/VLSI-EDA/PoC
+git clone https://github.com/rubund/qflow-1.2
+git clone https://github.com/rubund/qrouter
+git clone git://github.com/texane/stlink.git
+git clone https://github.com/rowol/stm32_discovery_arm_gcc
+git clone http://git.veripool.org/git/verilator
+git clone https://github.com/Vunit/vunit
+git clone https://github.com/cliffordwolf/yosys
+
