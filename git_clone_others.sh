06a4b44b6bf25b5698e14404bb299cebd2e797ff
Andreas Mueller <andreas.mueller2207@web.de>
2017-11-12 00:22:22 +0100

    New script to batch-clone my favorite git repos.

---
+++ git_clone_others.sh
@@ -0,0 +1,19 @@
+#!/bin/sh
+#
+# Date:   2016-08-02
+# Author: Andreas Mueller
+#
+# Description: Lazy script to clone some favored git repositories.
+#
+
+# Prio 1.
+git clone https://github.com/tgingold/ghdl
+git clone https://github.com/jandecaluwe/myhdl
+git clone https://github.com/jimlewis/osvvm
+git clone https://github.com/Vunit/vunit
+git clone https://github.com/VLSI-EDA/PoC
+git clone https://github.com/kevinpt/opbasm
+git clone https://github.com/philippfaes/grlib
+
+# Prio 2.
+git clone https://github.com/andygock/avr-uart
