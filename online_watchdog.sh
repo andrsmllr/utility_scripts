13cc1566153e66b09218ec3c166a0dc0eab47449
Andreas Mueller <andreas.mueller2207@web.de>
2018-03-17 16:18:22 +0100

    Commit local changes before wiping system.

--- online_watchdog.sh
+++ online_watchdog.sh
@@ -3,15 +3,15 @@
 # Test if online connection available by doing some nslookups.
 # Write log message about online status to log file.
 #
-log=online_watchdog.log
-test_sites=(google.ru amazon.de facebook.com)
-#for i in $(seq 1..3)
+log=~/.online_watchdog.log
+declare -a test_sites=(google.ru amazon.de facebook.com)
 timestamp=`date +%Y-%m-%d_%H:%M:%S`
-status=`nslookup google.de | grep answer`
-if [ "$status" == "" ]; then
-    printf "$timestamp : Online connection NOK!\n" >> $log
-else
-    printf "$timestamp : Online connection ok\n" >> $log
-fi
+for site in "${test_sites[@]}"; do
+    status=`nslookup $site | grep answer`
+    if [ "$status" == "" ]; then
+        printf "$timestamp : Online connection NOK! (nslookup for $site failed)\n" >> $log
+        exit 2
+    fi
+done
+printf "$timestamp : Online connection ok\n" >> $log
 exit 0
-
